b9ea7768c107939d6b15c9a2b2782c14e0a17db6
rebel-eunji <eunji.lee@rebellions.ai>
2025-07-13 18:23:41 +0900

    setting for v1 scheduler,worker,and runner

---
+++ vllm_rbln/core/optimum_scheduler_v1.py
@@ -0,0 +1,35 @@
+# Copyright 2025 Rebellions Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from vllm.v1.core.sched.scheduler import Scheduler
+
+from vllm_rbln.logger import init_logger
+
+logger = init_logger(__name__)
+
+
+class RBLNOptimumScheduler(Scheduler):
+    def __init__(
+        self,
+        vllm_config: VllmConfig,
+        kv_cache_config: KVCacheConfig,
+        structured_output_manager: StructuredOutputManager,
+        mm_registry: MultiModalRegistry = MULTIMODAL_REGISTRY,
+        include_finished_set: bool = False,
+        log_stats: bool = False,
+    ):
+        super().__init__(vllm_config, kv_cache_config,
+                         structured_output_manager, mm_registry,
+                         include_finished_set, log_stats)
+        # With V0 -> V1, block_manager is changed to kv_cache_manager
