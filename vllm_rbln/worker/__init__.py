786aadac84b7c72ff48c066377a6f9f7d0f76cdf
Youngkyu Choi <youngkyu.choi@rebellions.ai>
2025-07-16 19:01:09 +0900

    fix a bug where chunked prefill was not working

--- vllm_rbln/worker/__init__.py
+++ vllm_rbln/worker/__init__.py
@@ -14,7 +14,7 @@
 
 import vllm_rbln.attention.layer  # noqa
 import vllm_rbln.model_executor.layers.fused_moe.layer  # noqa
-import vllm_rbln.model_executor.layers.logits_processor # noqa
+import vllm_rbln.model_executor.layers.logits_processor  # noqa
 import vllm_rbln.model_executor.layers.rotary_embedding  # noqa
 import vllm_rbln.model_executor.model_loader.weight_loader  # noqa
 import vllm_rbln.models.deepseek_v2  # noqa
