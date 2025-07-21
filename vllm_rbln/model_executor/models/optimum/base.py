d119c91a21854eb32c669e30b10697808a7ddecd
rebel-eunji <eunji.lee@rebellions.ai>
2025-07-21 15:19:49 +0900

    qwen reranker

--- vllm_rbln/model_executor/models/optimum/base.py
+++ vllm_rbln/model_executor/models/optimum/base.py
@@ -38,6 +38,7 @@ _RBLN_GENERATION_MODELS: Dict[str, Tuple[str, str]] = {
     "ExaoneForCausalLM": ("exaone", "RBLNExaoneForCausalLM"),
     "Qwen2ForCausalLM": ("qwen2", "RBLNQwen2ForCausalLM"),
     "OPTForCausalLM": ("opt", "RBLNOPTForCausalLM"),
+    "Qwen3ForCausalLM": ("qwen3", "RBLNQwen3ForCausalLM"),
 }
 
 _RBLN_ENCODER_DECODER_MODELS: Dict[str, Tuple[str, str]] = {
