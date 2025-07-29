5e5281e61d0f8d025b02a1f563a8cc03437618c1
rebel-eunji <eunji.lee@rebellions.ai>
2025-07-29 11:57:57 +0900

    add logic

--- vllm_rbln/worker/optimum_worker.py
+++ vllm_rbln/worker/optimum_worker.py
@@ -155,10 +155,13 @@ class RBLNOptimumWorker(LoRANotSupportedWorkerBase,
         kvcache_partition_len = getattr(rbln_config, "kvcache_partition_len",
                                         None)
         max_seq_len = getattr(rbln_config, "max_seq_len", None)
+        dec_max_seq_len = getattr(rbln_config, "dec_max_seq_len", None)
+
         max_model_len = self.model_config.max_model_len
         max_num_seqs = self.scheduler_config.max_num_seqs
         block_size = self.vllm_config.cache_config.block_size
         max_num_batched_tokens = self.scheduler_config.max_num_batched_tokens
+
         # NOTE It is based on the decoder submodule is only one.
         for submodule in submodules:
             submodule_config = getattr(rbln_config, submodule)
@@ -166,15 +169,19 @@ class RBLNOptimumWorker(LoRANotSupportedWorkerBase,
                 kvcache_partition_len = getattr(submodule_config,
                                                 "kvcache_partition_len", None)
             if max_seq_len is None:
-                max_seq_len = getattr(
-                    submodule_config, "max_seq_len", None) or getattr(
-                        submodule_config, "dec_max_seq_len", None)
+                max_seq_len = getattr(submodule_config, "max_seq_len", None)
+
+            if dec_max_seq_len is None:
+                dec_max_seq_len = getattr(submodule_config, "dec_max_seq_len",
+                                          None)
+
             submodule_batch_size = getattr(submodule_config, "batch_size",
-                                        None)
+                                           None)
             # FIXME
             if submodule_batch_size is not None \
                 and batch_size != submodule_batch_size:
                 batch_size = submodule_batch_size
+
         if kvcache_partition_len is None:
             assert block_size == max_seq_len, (
                 f"`block_size({block_size})` must match "
@@ -185,7 +192,17 @@ class RBLNOptimumWorker(LoRANotSupportedWorkerBase,
                 f"`block_size({block_size})` must match "
                 f"the `kvcache_partition_len({kvcache_partition_len})` "
                 "of the compiled RBLN model.")
-        if max_seq_len:
+
+        if dec_max_seq_len is not None:  # encoder-decoder
+            assert max_num_batched_tokens == max_seq_len, (
+                f"`max_num_batched_tokens({max_num_batched_tokens})` "
+                f"must match the `dec_max_seq_len({dec_max_seq_len})` "
+                "of the compiled RBLM model.")
+            assert max_model_len == max_seq_len, (
+                f"`max_model_len({max_model_len})` must match "
+                f"the `dec_max_seq_len({dec_max_seq_len})` "
+                "of the compiled RBLM model.")
+        elif max_seq_len:  # including decoder
             assert max_num_batched_tokens == max_seq_len, (
                 f"`max_num_batched_tokens({max_num_batched_tokens})` "
                 f"must match the `max_seq_len({max_seq_len})` "
@@ -194,6 +211,7 @@ class RBLNOptimumWorker(LoRANotSupportedWorkerBase,
                 f"`max_model_len({max_model_len})` must match "
                 f"the `max_seq_len({max_seq_len})` "
                 "of the compiled RBLM model.")
+
         assert max_num_seqs == batch_size, (
             f"`max_num_seqs({max_num_seqs})` must match "
             f"the `batch_size({batch_size})` of the compiled RBLM model.")
