b3c668723d052b970b3fbef7787dbee6edd96556
rebel-eunji <eunji.lee@rebellions.ai>
2025-08-12 11:28:11 +0900

    add fixme

--- vllm_rbln/worker/optimum_model_runner.py
+++ vllm_rbln/worker/optimum_model_runner.py
@@ -387,6 +387,7 @@ class RBLNOptimumModelRunner(ModelRunnerBase[ModelInputForRBLN]):
                          lora_mapping: LoRAMapping) -> None:
         is_prefill = lora_mapping.is_prefill
         max_num_reqs = self.vllm_config.scheduler_config.max_num_seqs
+        # FIXME If the request does not have lora_request?
         num_reqs = len(lora_requests)
         adapter_ids = [
             lora_request.adapter_id for lora_request in lora_requests
