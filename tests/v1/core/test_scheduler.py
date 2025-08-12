35ca2565caf28a5524e7237adba20f81af57bba5
seinpark <seinpark@rebellions.ai>
2025-08-12 18:43:32 +0900

    feat: add licence

--- tests/v1/core/test_scheduler.py
+++ tests/v1/core/test_scheduler.py
@@ -1,5 +1,17 @@
-# SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# Copyright 2025 Rebellions Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Optional
 
 import pytest
@@ -301,13 +313,3 @@ def test_schedule_preempted_block(
     assert scheduler_output2.scheduled_cached_reqs[0].new_block_ids[0] == [
         4
     ]  # preempted_block
-
-
-# TODO(si) remove
-if __name__ == "__main__":
-    # test_add_requests()
-    # test_finish_request()
-    # test_schedule_single_seq(None, None)
-    # test_schedule_multi_seq(None, None)
-    # test_schedule_alloc_block(False, None, 2, 16, 32, 64, 8, 32)
-    test_schedule_preempted_block(False, None, 2, 16, 32, 64, 5, 32)
