bade0b23df05f728b79eab88ac0481de38d8a125
rebel-eunji <eunji.lee@rebellions.ai>
2025-08-19 10:43:09 +0900

    fix pre-commit

--- vllm_rbln/model_executor/models/optimum/optimum_attention/__init__.py
+++ vllm_rbln/model_executor/models/optimum/optimum_attention/__init__.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Rebellions Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .optimum_attention_manager import (AttentionManager,
                                         HybridAttentionImageManager)
 from .optimum_attention_strategy import (HybridAttentionImageStrategy,
@@ -6,6 +20,7 @@ from .optimum_attention_strategy import (HybridAttentionImageStrategy,
                                          InnerR2)
 
 __all__ = [
-    AttentionManager, InnerAttentionEntry, InnerAttentionStrategy, InnerR1,
-    InnerR2, HybridAttentionImageManager, HybridAttentionImageStrategy
+    "AttentionManager", "InnerAttentionEntry", "InnerAttentionStrategy",
+    "InnerR1", "InnerR2", "HybridAttentionImageManager",
+    "HybridAttentionImageStrategy"
 ]
