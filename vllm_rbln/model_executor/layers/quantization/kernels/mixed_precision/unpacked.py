bd02310822b6fce851dfb158871e03cce5cdc584
Jaehwang Jung <jaehwang.jung@rebellions.ai>
2025-08-22 16:39:00 +0900

    Torch implementation of group quant and activation ordering

--- vllm_rbln/model_executor/layers/quantization/kernels/mixed_precision/unpacked.py
+++ vllm_rbln/model_executor/layers/quantization/kernels/mixed_precision/unpacked.py
@@ -25,6 +25,11 @@ from compressed_tensors.compressors.quantized_compressors import unpack_from_int
 
 
 class RBLNInt8UnpackedLinearKernel(MPLinearKernel):
+    """
+    Torch native implementation of mixed precision Linear, based on
+    compressed_tensors' dequantize() function. rebel_compiler detects this
+    pattern and maps it to the actual kernel.
+    """
 
     @classmethod
     def get_min_capability(cls) -> int:
@@ -44,14 +49,22 @@ class RBLNInt8UnpackedLinearKernel(MPLinearKernel):
         return True, None
 
     def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
-        bits = self.config.weight_type.mantissa
+        c = self.config
+        bits = c.weight_type.mantissa
 
         os.environ['RBLN_QUANT_BITS'] = str(bits)
 
+        if c.has_g_idx:
+            w_gidx = getattr(layer, self.w_gidx_name)
+            layer.perm = torch.argsort(w_gidx)
+
         def transform_w_q(x: BasevLLMParameter):
+            in_features, out_features = c.full_weight_shape
             x.data = unpack_from_int32(
                 x.data, bits,
-                torch.Size((x.shape[0], x.shape[1] * (32 // bits))))
+                torch.Size((out_features, in_features)))
+            if c.has_g_idx:
+                x.data = x.data[:, layer.perm]
             return x
 
         self._transform_param(layer, self.w_q_name, transform_w_q)
@@ -61,7 +74,18 @@ class RBLNInt8UnpackedLinearKernel(MPLinearKernel):
                       layer: torch.nn.Module,
                       x: torch.Tensor,
                       bias: Optional[torch.Tensor] = None) -> torch.Tensor:
+        in_features, out_features = self.config.full_weight_shape
+        group_size = self.config.group_size if self.config.group_size > 0 else None
+
+        if self.config.has_g_idx:
+            x = x[..., layer.perm]
+
         w_q, w_s, _, _ = self._get_weight_params(layer)
-        w_fp = w_q.type(x.dtype)
-        w_fp *= w_s.view(-1, 1)
+        if group_size is not None:
+            w_q = w_q.view(out_features, -1, group_size)
+            w_fp = w_q.type(x.dtype) * w_s.unsqueeze(-1)
+            w_fp = w_fp.view(out_features, in_features)
+        else:
+            w_fp = w_q.type(x.dtype) * w_s
+
         return torch.nn.functional.linear(x, w_fp, bias)
