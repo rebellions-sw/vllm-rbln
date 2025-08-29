28d22dc2827b5372708e1313308d3375254817d8
Jiwoo Park <jiwoo.park@rebellions.ai>
2025-08-29 01:24:44 +0000

    Fix kv cache mark static.

--- vllm_rbln/worker/model_runner.py
+++ vllm_rbln/worker/model_runner.py
@@ -631,8 +631,9 @@ class RBLNModelRunner(ModelRunnerBase[ModelInputForRebelWithSamplingMetadata]):
             # RBLN compile context is much similar to vLLM forward context
             if model_input.attn_metadata is not None:
                 model_input.attn_metadata.kv_caches = kv_caches
-            # for kv_cache in kv_caches:
-            #     self.compile_context.mark_static_address(kv_cache)
+            if not self.model_config.enforce_eager and envs.RBLN_COMPILE_MODEL:
+                for kv_cache in kv_caches:
+                    self.compile_context.mark_static_address(kv_cache)
             hidden_states = self.model_executable(
                 input_ids=model_input.input_tokens,
                 positions=model_input.input_positions,
