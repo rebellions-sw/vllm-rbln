15910420b48e9a9ee04d3a5bfebad0a3ebb496b4
rebel-eunji <eunji.lee@rebellions.ai>
2025-10-15 16:08:29 +0900

    change the default block_size

--- scripts/validation/compare_logprobs_advanced.py
+++ scripts/validation/compare_logprobs_advanced.py
@@ -41,9 +41,9 @@ def generate_llm_args(device):
         "max_logprobs": VOCAB_SIZE,
     }
     if device == "cpu":
-        llm_args["block_size"] = 16
+        llm_args["block_size"] = 128
     elif device == "rbln":
-        llm_args["block_size"] = 1024
+        llm_args["block_size"] = 128 # 1024 is not working for long prompt
         llm_args["max_num_batched_tokens"] = PREFILL_CHUNK_SIZE
     return llm_args
 
