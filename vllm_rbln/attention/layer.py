fc57071e0a821919eb8f09a569a73848239d5114
Jiwoo Park <jiwoo.park@rebellions.ai>
2025-10-20 17:21:34 +0900

    update: Modify code to be compatible with v0.10.2

--- vllm_rbln/attention/layer.py
+++ vllm_rbln/attention/layer.py
@@ -12,15 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib.util
-from typing import Any, Dict, List, Optional
+from typing import List, Optional
 
 import torch
 import torch.nn as nn
 import vllm.envs as envs
 from vllm.attention import AttentionType
+from vllm.attention.backends.abstract import AttentionBackend
 from vllm.attention.layer import Attention
 from vllm.attention.selector import backend_name_to_enum, get_attn_backend
+from vllm.attention.utils.kv_sharing_utils import validate_kv_sharing_target
 from vllm.config import CacheConfig, get_current_vllm_config
 from vllm.forward_context import ForwardContext, get_forward_context
 from vllm.model_executor.layers.linear import UnquantizedLinearMethod
@@ -43,13 +44,13 @@ def __custom_init__(
     alibi_slopes: Optional[List[float]] = None,
     cache_config: Optional[CacheConfig] = None,
     quant_config: Optional[QuantizationConfig] = None,
-    blocksparse_params: Optional[Dict[str, Any]] = None,
     logits_soft_cap: Optional[float] = None,
     per_layer_sliding_window: Optional[int] = None,
     use_mla: bool = False,
     prefix: str = "",
     attn_type: str = AttentionType.DECODER,
     kv_sharing_target_layer_name: Optional[str] = None,
+    attn_backend: Optional[type[AttentionBackend]] = None,
     **extra_impl_args,
 ) -> None:
     """
@@ -79,6 +80,9 @@ def __custom_init__(
         calculate_kv_scales = False
     if num_kv_heads is None:
         num_kv_heads = num_heads
+    assert num_heads % num_kv_heads == 0, \
+        f"num_heads ({num_heads}) is not " \
+        f"divisible by num_kv_heads ({num_kv_heads})"
 
     # The default k/v_scale is set to 1.0. This is ignored
     # when kv-cache is not fp8, and should be used with
@@ -94,16 +98,23 @@ def __custom_init__(
     self._q_scale = torch.tensor(1.0, dtype=torch.float32)
     self._prob_scale = torch.tensor(1.0, dtype=torch.float32)
 
-    # We also keep the float32 versions of k/v_scale for attention
-    # backends that don't support tensors (Flashinfer)
+    # We also keep q/k/v_scale on host (cpu) memory for attention
+    # backends that require the scales to be on host instead of on device.
+    # e.g. Flashinfer
+    self._q_scale_float = 1.0
     self._k_scale_float = 1.0
     self._v_scale_float = 1.0
+
+    # The output scale on host memory. This should be the input scale of
+    # the quant op after this attention layer.
+    self._o_scale_float: Optional[float] = None
 
     self.use_mla = use_mla
     self.num_heads = num_heads
     self.head_size = head_size
     self.num_kv_heads = num_kv_heads
     self.sliding_window = sliding_window
+    self.has_sink = extra_impl_args.get("sinks") is not None
 
     quant_method = quant_config.get_quant_method(
         self, prefix=prefix) if quant_config else None
@@ -113,8 +124,8 @@ def __custom_init__(
         # TODO (mgoin): kv cache dtype should be specified in the FP8
         # checkpoint config and become the "auto" behavior
         if self.kv_cache_dtype == "fp8_e5m2":
-            raise ValueError(
-                "fp8_e5m2 kv-cache is not supported with fp8 checkpoints.")
+            raise ValueError("fp8_e5m2 kv-cache is not supported with "
+                             "fp8 checkpoints.")
         # If quantization is enabled, we make "k_scale" and "v_scale"
         # parameters so that it can be loaded from the model checkpoint.
         # The k/v_scale will then be converted back to native float32
@@ -125,39 +136,32 @@ def __custom_init__(
     # During model initialization, the default dtype is set as the model
     # weight and activation dtype.
     dtype = torch.get_default_dtype()
-    attn_backend = get_attn_backend(head_size,
-                                    dtype,
-                                    kv_cache_dtype,
-                                    block_size,
-                                    is_attention_free,
-                                    blocksparse_params is not None,
-                                    use_mla=use_mla)
-    impl_cls = attn_backend.get_impl_cls()
-    self.impl = impl_cls(
-        num_heads,
-        head_size,
-        scale,
-        num_kv_heads,
-        alibi_slopes,
-        sliding_window,
-        kv_cache_dtype,
-        blocksparse_params,
-        logits_soft_cap,
-        attn_type,
-        kv_sharing_target_layer_name,
-        **extra_impl_args,
-    )
-    self.backend = backend_name_to_enum(attn_backend.get_name())
+    if attn_backend is None:
+        self.attn_backend = get_attn_backend(head_size,
+                                             dtype,
+                                             kv_cache_dtype,
+                                             block_size,
+                                             is_attention_free,
+                                             use_mla=use_mla,
+                                             has_sink=self.has_sink)
+    else:
+        self.attn_backend = attn_backend
+
+    impl_cls = self.attn_backend.get_impl_cls()
+    self.impl = impl_cls(num_heads, head_size, scale, num_kv_heads,
+                         alibi_slopes, sliding_window, kv_cache_dtype,
+                         logits_soft_cap, attn_type,
+                         kv_sharing_target_layer_name, **extra_impl_args)
+    self.backend = backend_name_to_enum(self.attn_backend.get_name())
     self.dtype = dtype
 
     # For cuda-alike (CUDA and ROCM) and cpu platforms, we control how
     # torch.compile works by registering the attention as one giant
     # opaque custom op. For other platforms, we directly call them
     # and let torch.compile handle them.
-    self.use_direct_call = not current_platform.is_cuda_alike(
-    ) and not current_platform.is_cpu()
+    self.use_direct_call = not current_platform.opaque_attention_op()
 
-    self.use_output = attn_backend.accept_output_buffer
+    self.use_output = self.attn_backend.accept_output_buffer
     compilation_config = get_current_vllm_config().compilation_config
     if prefix in compilation_config.static_forward_context:
         raise ValueError(f"Duplicate layer name: {prefix}")
@@ -166,19 +170,11 @@ def __custom_init__(
     self.attn_type = attn_type
 
     if kv_sharing_target_layer_name is not None:
-        if not envs.VLLM_USE_V1:
-            raise NotImplementedError(
-                "Cross-layer KV sharing is not supported in V0.")
-
-        if importlib.util.find_spec(
-                "vllm.v1.attention.backends.utils") is not None:
-            from vllm.v1.attention.backends.utils import (
-                validate_kv_sharing_target)
-            validate_kv_sharing_target(
-                prefix,
-                kv_sharing_target_layer_name,
-                compilation_config.static_forward_context,
-            )
+        validate_kv_sharing_target(
+            prefix,
+            kv_sharing_target_layer_name,
+            compilation_config.static_forward_context,
+        )
     self.kv_sharing_target_layer_name = kv_sharing_target_layer_name
 
     # use a placeholder kv cache tensor during init, which will be replaced
@@ -229,8 +225,9 @@ def custom_attention_forward(
         if attn_metadata.enable_kv_scales_calculation:
             self.calc_kv_scales(query, key, value)
     if self.use_output:
-        output_shape = output_shape if output_shape is not None else query.shape
-        output = torch.empty(output_shape,
+        output_shape = (output_shape
+                        if output_shape is not None else query.shape)
+        output = torch.zeros(output_shape,
                              dtype=query.dtype,
                              device=query.device)
         hidden_size = output_shape[-1]
