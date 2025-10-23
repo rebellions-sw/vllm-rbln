1c79e66acf985d21fb1891baa457c936204107ee
rebel-eunji <eunji.lee@rebellions.ai>
2025-10-23 16:21:18 +0900

    missing function

--- vllm_rbln/v1/worker/optimum_model_runner.py
+++ vllm_rbln/v1/worker/optimum_model_runner.py
@@ -407,6 +407,32 @@ class RBLNOptimumModelRunner(LoRAModelRunnerMixin):
             )
         return kv_cache_spec
 
+    def _extract_mm_kwargs(
+        self,
+        scheduler_output: "SchedulerOutput",
+    ) -> BatchedTensorInputs:
+        if not scheduler_output or not self.is_multimodal_raw_input_only_model:
+            return {}
+
+        mm_kwargs = list[MultiModalKwargsItem]()
+        for req in scheduler_output.scheduled_new_reqs:
+            for feature in req.mm_features:
+                if feature.data is not None:
+                    mm_kwargs.append(feature.data)
+
+        # Input all modalities at once
+        model = cast(SupportsMultiModal, self.model)
+        mm_kwargs_combined: BatchedTensorInputs = {}
+        for _, _, mm_kwargs_group in group_mm_kwargs_by_modality(
+            mm_kwargs,
+            device=self.device,
+            pin_memory=self.pin_memory,
+            merge_by_field_config=model.merge_by_field_config,
+        ):
+            mm_kwargs_combined.update(mm_kwargs_group)
+
+        return mm_kwargs_combined
+
     def _prepare_prefill(
         self,
         scheduler_output: "SchedulerOutput",
