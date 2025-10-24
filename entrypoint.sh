9599fd38f3594e32b5d33f5c1fdd74e3b94cceb2
rebel-jaebin <jaebin@rebellions.ai>
2025-10-24 16:49:34 +0900

    Add files for CI

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Prepare host modules and udev triggers
+depmod -a $(uname -r) 2>/dev/null || true
+udevadm control --reload 2>/dev/null || true
+udevadm trigger 2>/dev/null || true
+
+# Execute user-specified command
+exec "$@"
+
