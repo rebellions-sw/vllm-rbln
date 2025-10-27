730e6aeb7f5feb2e656c891157dd409084e4d1bd
rebel-jaebin <jaebin@rebellions.ai>
2025-10-27 16:20:50 +0900

    Reflect comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,9 @@
 set -e
 
 # Prepare host modules and udev triggers
-depmod -a $(uname -r) 2>/dev/null || true
+depmod -a "$(uname -r)" 2>/dev/null || true
 udevadm control --reload 2>/dev/null || true
 udevadm trigger 2>/dev/null || true
 
 # Execute user-specified command
 exec "$@"
-
