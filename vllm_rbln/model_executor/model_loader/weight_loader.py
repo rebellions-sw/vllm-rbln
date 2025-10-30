1d3436e7209161b3405aae004a7a7cc271036156
Jaehwang Jung <jaehwang.jung@rebellions.ai>
2025-10-30 13:44:08 +0900

    hack: gemma3 num_hidden_layers weight loading

--- vllm_rbln/model_executor/model_loader/weight_loader.py
+++ vllm_rbln/model_executor/model_loader/weight_loader.py
@@ -20,8 +20,8 @@ from vllm.logger import init_logger
 from vllm.model_executor.layers.fused_moe import FusedMoE
 from vllm.model_executor.model_loader.weight_utils import (
     default_weight_loader, maybe_remap_kv_scale_name)
-from vllm.model_executor.models import (deepseek_v2, llama, llama4, qwen2,
-                                        qwen2_moe, qwen3_moe, utils)
+from vllm.model_executor.models import (deepseek_v2, gemma3, llama, llama4,
+                                        qwen2, qwen2_moe, qwen3_moe, utils)
 
 logger = init_logger(__name__)
 
@@ -578,6 +578,65 @@ def load_llama4_weights(
     return loaded_params
 
 
+def load_gemma3_weights(
+        self, weights: Iterable[tuple[str, torch.Tensor]]) -> set[str]:
+    stacked_params_mapping = [
+        # (param_name, shard_name, shard_id)
+        ("qkv_proj", "q_proj", "q"),
+        ("qkv_proj", "k_proj", "k"),
+        ("qkv_proj", "v_proj", "v"),
+        ("gate_up_proj", "gate_proj", 0),
+        ("gate_up_proj", "up_proj", 1),
+    ]
+    params_dict = dict(self.named_parameters())
+    loaded_params: set[str] = set()
+    for name, loaded_weight in weights:
+        if name.startswith("layers"):
+            layer_idx = int(name.split(".")[1])
+            if layer_idx >= self.config.num_hidden_layers:
+                continue
+        if (self.quant_config is not None
+                and (scale_name := self.quant_config.get_cache_scale(name))):
+            # Loading kv cache scales for compressed-tensors quantization
+            param = params_dict[scale_name]
+            weight_loader = getattr(param, "weight_loader",
+                                    default_weight_loader)
+            loaded_weight = loaded_weight[0]
+            weight_loader(param, loaded_weight)
+            loaded_params.add(scale_name)
+            continue
+        for (param_name, shard_name, shard_id) in stacked_params_mapping:
+            if shard_name not in name:
+                continue
+            name = name.replace(shard_name, param_name)
+            # Skip loading extra bias for GPTQ models.
+            if name.endswith(".bias") and name not in params_dict:
+                continue
+            if utils.is_pp_missing_parameter(name, self):
+                continue
+            param = params_dict[name]
+            weight_loader = param.weight_loader
+            weight_loader(param, loaded_weight, shard_id)
+            break
+        else:
+            # Skip loading extra bias for GPTQ models.
+            if name.endswith(".bias") and name not in params_dict:
+                continue
+            # Remapping the name of FP8 kv-scale.
+            name = maybe_remap_kv_scale_name(name, params_dict)
+            if name is None:
+                continue
+            if utils.is_pp_missing_parameter(name, self):
+                continue
+            param = params_dict[name]
+            weight_loader = getattr(param, "weight_loader",
+                                    default_weight_loader)
+            weight_loader(param, loaded_weight)
+        loaded_params.add(name)
+
+    return loaded_params
+
+
 llama.LlamaModel.load_weights = load_llama_weights
 llama4.Llama4Model.load_weights = load_llama4_weights
 
@@ -585,3 +644,4 @@ qwen2.Qwen2Model.load_weights = load_qwen2_weights
 qwen2_moe.Qwen2MoeModel.load_weights = load_qwen2moe_weights
 qwen3_moe.Qwen3MoeModel.load_weights = load_qwen3moe_weights
 deepseek_v2.DeepseekV2ForCausalLM.load_weights = load_deepseek_v2_weights
+gemma3.Gemma3Model.load_weights = load_gemma3_weights
