ad964ebbbecfa22515d821287bb71419e00cac7f
rebel-eunji <eunji.lee@rebellions.ai>
2025-11-11 22:21:14 +0900

    feat(model): Support for Structured Ouput (#151)

---
+++ tests/v1/core/test_structured_output.py
@@ -0,0 +1,45 @@
+# Copyright 2025 Rebellions Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+from .utils import create_requests, create_scheduler
+
+
+def test_structured_output():
+    scheduler = create_scheduler()
+    structured_output_manager = scheduler.structured_output_manager
+    sample_json_schema = {"a": "int", "b": {"c": "str", "d": "float"}}
+    str_sample_json_schema = json.dumps(sample_json_schema)
+    requests = create_requests(num_requests=2,
+                               sample_json_schema=str_sample_json_schema)
+    for request in requests:
+        scheduler.add_request(request)
+        structured_output_manager.grammar_init(request)
+        # The grammar might not yet be compiled, so we wait for it
+        while not request.structured_output_request._check_grammar_completion(
+        ):
+            continue
+
+    # Prefill 1st request
+    scheduler_output = scheduler.schedule()
+    assert len(scheduler_output.structured_output_request_ids) == 1
+    assert len(scheduler_output.grammar_bitmask) == 1
+    # Prefill 2nd request
+    scheduler_output = scheduler.schedule()
+    assert len(scheduler_output.structured_output_request_ids) == 1
+    assert len(scheduler_output.grammar_bitmask) == 1
+    # Decode step for both requests
+    scheduler_output = scheduler.schedule()
+    assert len(scheduler_output.structured_output_request_ids) == 2
--- vllm_rbln/v1/core/optimum_scheduler.py
+++ vllm_rbln/v1/core/optimum_scheduler.py
@@ -393,6 +393,10 @@ class RBLNOptimumScheduler(Scheduler):
             scheduled_spec_decode_tokens,
             req_to_new_blocks,
         )
+        structured_output_request_ids, grammar_bitmask = (
+            self.get_grammar_bitmask(
+                scheduled_new_reqs + scheduled_running_reqs,
+                scheduled_spec_decode_tokens))
 
         scheduler_output = RBLNSchedulerOutput(
             scheduled_new_reqs=new_reqs_data,
@@ -408,8 +412,8 @@ class RBLNOptimumScheduler(Scheduler):
             # the previous and the current steps.
             finished_req_ids=self.finished_req_ids,
             free_encoder_mm_hashes=[],
-            structured_output_request_ids={},
-            grammar_bitmask=None,
+            structured_output_request_ids=structured_output_request_ids,
+            grammar_bitmask=grammar_bitmask,
             new_computed_blocks=new_computed_blocks.get_block_ids()[0],
             preempted_req_ids=[req.request_id for req in preempted_reqs],
         )
