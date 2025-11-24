fe7e5a3972c831db52eaf8d71f6ba23fc86a7b40
rebel-eunji <eunji.lee@rebellions.ai>
2025-11-24 14:51:47 +0900

    fix qwen2 vl

--- vllm_rbln/model_executor/models/optimum/qwen_vl.py
+++ vllm_rbln/model_executor/models/optimum/qwen_vl.py
@@ -72,16 +72,17 @@ class RBLNOptimumQwenVLForConditionalGeneration(RBLNOptimumModelBase,
         """
         # NOTE(eunji.lee): It is a patch for bfloat16 support.
         dtype = self.rbln_model_config.torch_dtype
-        if image_input is not None and image_input[
-                "pixel_values"] is not None and dtype != image_input[
-                    "pixel_values"].dtype:
-            image_input["pixel_values"] = image_input["pixel_values"].to(dtype)
-        if video_input is not None and video_input[
-                "pixel_values_videos"] is not None and dtype != video_input[
-                    "pixel_values_videos"].dtype:
-            video_input["pixel_values_videos"] = video_input[
-                "pixel_values_videos"].to(dtype)
 
+        if image_input is not None:
+            assert image_input["pixel_values"] is not None
+            pixel_values = image_input["pixel_values"]
+            if dtype != pixel_values.dtype:
+                pixel_values = pixel_values.to(dtype)
+        if video_input is not None:
+            assert video_input["pixel_values_videos"] is not None
+            pixel_values_videos = video_input["pixel_values_videos"]
+            if dtype != pixel_values_videos.dtype:
+                pixel_values_videos = pixel_values_videos.to(dtype)
         # Prepare base arguments common to all models
         preprocess_args = {
             "input_ids":
@@ -89,12 +90,11 @@ class RBLNOptimumQwenVLForConditionalGeneration(RBLNOptimumModelBase,
             "attention_mask":
             attention_mask,
             "pixel_values":
-            image_input["pixel_values"] if image_input is not None else None,
+            pixel_values if image_input is not None else None,
             "image_grid_thw":
             image_input["image_grid_thw"] if image_input is not None else None,
             "pixel_values_videos":
-            video_input["pixel_values_videos"]
-            if video_input is not None else None,
+            pixel_values_videos if video_input is not None else None,
             "video_grid_thw":
             video_input["video_grid_thw"] if video_input is not None else None,
         }
