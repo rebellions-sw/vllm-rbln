18b30372a4495d7ddaac68f6473d171a6cc36266
rebel-eunji <eunji.lee@rebellions.ai>
2025-12-01 20:50:27 +0900

    remove options of torch.compile

--- vllm_rbln/v1/sample/rbln_sampler.py
+++ vllm_rbln/v1/sample/rbln_sampler.py
@@ -109,15 +109,11 @@ class RBLNSampler(VLLMSampler):
                  seed: int = 42):
         super().__init__()
         rebel.manual_seed(seed)
-        options = {
-            "cache_dir": "./cache_dir",
-        }
         self._compiled_rbln_topp_sampler = torch.compile(
             self._rbln_topp_sampler_impl,
             dynamic=False,
             fullgraph=True,
             backend="rbln",
-            options=options,
         )
         self.logprobs_mode = logprobs_mode
 
