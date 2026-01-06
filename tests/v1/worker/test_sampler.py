d4096f117e2cae4326b3af59ce65d10fdbf8c5c0
rebel-eunji <eunji.lee@rebellions.ai>
2026-01-06 10:07:16 +0900

    off other tests

--- tests/v1/worker/test_sampler.py
+++ tests/v1/worker/test_sampler.py
@@ -16,8 +16,9 @@ import pytest
 import torch
 from vllm.platforms import current_platform
 
-from .utils import (create_model_runner, fake_load_model, forward_steps,
-                    make_request)
+# from .utils import (create_model_runner, fake_load_model, forward_steps,
+#                     make_request)
+from .utils import forward_steps, make_request
 
 DEVICE = current_platform.device_type
 
@@ -28,63 +29,70 @@ def dynamo_reset():
     torch._dynamo.reset()
 
 
-@pytest.mark.parametrize("num_seqs, expected_bucket_sizes", [
-    pytest.param(1, [1], id="1_seq"),
-    pytest.param(2, [1, 2], id="2_seq"),
-    pytest.param(16, [1, 2, 4, 8, 16], id="16_seq"),
-    pytest.param(17, [1, 2, 4, 8, 16, 17], id="17_seq"),
-    pytest.param(61, [1, 2, 4, 8, 16, 24, 32, 40, 48, 56, 61], id="64_seq"),
-    pytest.param(512, [
-        1, 2, 4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120,
-        128, 136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232,
-        240, 248, 256, 272, 288, 304, 320, 336, 352, 368, 384, 400, 416, 432,
-        448, 464, 480, 496, 512
-    ],
-                 id="512_seq"),
-    pytest.param(515, [
-        1, 2, 4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120,
-        128, 136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232,
-        240, 248, 256, 272, 288, 304, 320, 336, 352, 368, 384, 400, 416, 432,
-        448, 464, 480, 496, 512, 515
-    ],
-                 id="515_seq"),
-])
-def test_get_bucket_sizes(monkeypatch, num_seqs: int,
-                          expected_bucket_sizes: list[int]):
-    monkeypatch.setenv("VLLM_RBLN_SAMPLER", "1")
-    runner = create_model_runner(max_num_seqs=num_seqs)
-    fake_load_model(runner)
-    bucket_sizes = runner.get_bucket_sizes(num_seqs)
-    assert bucket_sizes == expected_bucket_sizes
-    assert len(runner.pooled_tensors) == len(expected_bucket_sizes)
+# @pytest.mark.parametrize("num_seqs, expected_bucket_sizes", [
+#     pytest.param(1, [1], id="1_seq"),
+#     pytest.param(2, [1, 2], id="2_seq"),
+#     pytest.param(16, [1, 2, 4, 8, 16], id="16_seq"),
+#     pytest.param(17, [1, 2, 4, 8, 16, 17], id="17_seq"),
+#     pytest.param(61, [1, 2, 4, 8, 16, 24, 32, 40, 48, 56, 61], id="64_seq"),
+#     pytest.param(512, [
+#         1, 2, 4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120,
+#         128, 136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232,
+#         240, 248, 256, 272, 288, 304, 320, 336, 352, 368, 384, 400, 416, 432,
+#         448, 464, 480, 496, 512
+#     ],
+#                  id="512_seq"),
+#     pytest.param(515, [
+#         1, 2, 4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120,
+#         128, 136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232,
+#         240, 248, 256, 272, 288, 304, 320, 336, 352, 368, 384, 400, 416, 432,
+#         448, 464, 480, 496, 512, 515
+#     ],
+#                  id="515_seq"),
+# ])
+# def test_get_bucket_sizes(monkeypatch, num_seqs: int,
+#                           expected_bucket_sizes: list[int]):
+#     monkeypatch.setenv("VLLM_RBLN_SAMPLER", "1")
+#     runner = create_model_runner(max_num_seqs=num_seqs)
+#     fake_load_model(runner)
+#     bucket_sizes = runner.get_bucket_sizes(num_seqs)
+#     assert bucket_sizes == expected_bucket_sizes
+#     assert len(runner.pooled_tensors) == len(expected_bucket_sizes)
+
+# @pytest.mark.parametrize("use_rbln_sampler", [False])
+# @pytest.mark.parametrize("use_structured_output", [True, False])
+# def test_forward_sampler_mode_and_structured_output(monkeypatch,
+#                                                     use_rbln_sampler,
+#                                                     use_structured_output):
+#     """Test sampler logic for both use_rbln_sampler=True and False."""
+#     monkeypatch.setenv("VLLM_RBLN_COMPILE_STRICT_MODE", "1")
+#     monkeypatch.setenv("VLLM_RBLN_SAMPLER", "1" if use_rbln_sampler else "0")
+#     reqs = []
+#     for i in range(3):
+#         reqs.append(
+#             make_request(request_id=f"req_{i}",
+#                          prompt_token_ids=[1, 2, 3],
+#                          use_structured_output=use_structured_output,
+#                          top_p=0.7))
+#     forward_steps(reqs)
 
 
-@pytest.mark.parametrize("use_rbln_sampler", [False])
-@pytest.mark.parametrize("use_structured_output", [True, False])
-def test_forward_sampler_mode_and_structured_output(monkeypatch,
-                                                    use_rbln_sampler,
-                                                    use_structured_output):
-    """Test sampler logic for both use_rbln_sampler=True and False."""
-    monkeypatch.setenv("VLLM_RBLN_COMPILE_STRICT_MODE", "1")
-    monkeypatch.setenv("VLLM_RBLN_SAMPLER", "1" if use_rbln_sampler else "0")
-    reqs = []
-    for i in range(3):
-        reqs.append(
-            make_request(request_id=f"req_{i}",
-                         prompt_token_ids=[1, 2, 3],
-                         use_structured_output=use_structured_output,
-                         top_p=0.7))
-    forward_steps(reqs)
-
-
-@pytest.mark.parametrize("use_structured_output", [True, False])
-@pytest.mark.parametrize("top_p", [0.7, 1.0])
-@pytest.mark.parametrize("top_k", [0, 3])
+# @pytest.mark.parametrize("use_structured_output", [True, False])
+# @pytest.mark.parametrize("top_p", [0.7, 1.0])
+# @pytest.mark.parametrize("top_k", [0, 3])
+# @pytest.mark.parametrize("temperature", [0.0, 1.0])
+# @pytest.mark.parametrize("logprobs", [0, 3])
+# @pytest.mark.parametrize("presence_penalty", [-2.0, 0.0, 2.0])
+# @pytest.mark.parametrize("frequency_penalty", [-2.0, 0.0, 2.0])
+# @pytest.mark.parametrize("repetition_penalty", [1.0, 2.0])
+# @pytest.mark.parametrize("use_structured_output", [True, False])
+@pytest.mark.parametrize("top_p", [1.0])
+# @pytest.mark.parametrize("top_k", [0, 3])
 @pytest.mark.parametrize("temperature", [0.0, 1.0])
-@pytest.mark.parametrize("logprobs", [0, 3])
-@pytest.mark.parametrize("presence_penalty", [-2.0, 0.0, 2.0])
-@pytest.mark.parametrize("frequency_penalty", [-2.0, 0.0, 2.0])
-@pytest.mark.parametrize("repetition_penalty", [1.0, 2.0])
+# @pytest.mark.parametrize("logprobs", [0, 3])
+# @pytest.mark.parametrize("presence_penalty", [-2.0, 0.0, 2.0])
+# @pytest.mark.parametrize("frequency_penalty", [-2.0, 0.0, 2.0])
+# @pytest.mark.parametrize("repetition_penalty", [1.0, 2.0])
 def test_forward_sampling_parameters(monkeypatch, use_structured_output, top_p,
                                      top_k, temperature, logprobs,
                                      presence_penalty, frequency_penalty,
