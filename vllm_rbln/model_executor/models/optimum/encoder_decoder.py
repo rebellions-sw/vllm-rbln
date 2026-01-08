aaba0b269bdcfc1b62040d0e239e163034a90dcf
rebel-eunji <eunji.lee@rebellions.ai>
2026-01-08 12:30:45 +0900

    fix model_base preprocess

--- vllm_rbln/model_executor/models/optimum/encoder_decoder.py
+++ vllm_rbln/model_executor/models/optimum/encoder_decoder.py
@@ -36,9 +36,8 @@ class RBLNOptimumEncoderDecoder(RBLNOptimumModelBase, RBLNOptimumDecoderMixin):
         self.enc_lengths = [0] * self.batch_size
         self.setup_decoder_mixin(
             attn_impl=self.attn_impl,
-            vocab_size=self.model_config.get_vocab_size,
+            vllm_config=self.vllm_config,
             use_multiple_decoder=False,
-            default_batch_size=self.scheduler_config.max_num_seqs,
             decoder_batch_sizes=[self.batch_size],
             num_blocks=self.kv_block_adapter._estimated_num_blocks(),
         )
