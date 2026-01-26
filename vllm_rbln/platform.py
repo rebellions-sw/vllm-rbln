d362e41d06e658f1dc909e58de6b7a6642ea09ec
rebel-eunji <eunji.lee@rebellions.ai>
2026-01-26 15:49:15 +0900

    pre-commit

--- vllm_rbln/platform.py
+++ vllm_rbln/platform.py
@@ -202,7 +202,8 @@ class RblnPlatform(Platform):
 
         assert (not vllm_config.speculative_config
                 ), "Speculative decoding not yet supported for RBLN backend."
-        assert not envs.VLLM_USE_V2_MODEL_RUNNER, "v2 model runner is not supported for RBLN backend."
+        assert not envs.VLLM_USE_V2_MODEL_RUNNER, \
+            "v2 model runner is not supported for RBLN backend."
 
         if envs.VLLM_RBLN_USE_VLLM_MODEL:
             from vllm.config import CompilationLevel
