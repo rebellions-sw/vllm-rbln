fb648ed9a2a9d1c812ccac3ffe551eb6954aa855
rebel-ykchoi <youngkyu.choi@rebellions.ai>
2026-02-03 09:44:17 +0900

    fix dummy run block id (#323)
    * fix dummy run block id
    
    vllm manages dummy block id as zero, so we need to use it for dummy run,
    not the last block id.
    
    * remove useless param
    
    ---------
    
    Co-authored-by: Jaehwang Jung <jaehwang.jung@rebellions.ai>

--- vllm_rbln/v1/worker/rbln_model_runner.py
+++ vllm_rbln/v1/worker/rbln_model_runner.py
@@ -1702,12 +1702,13 @@ class RBLNModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
         sampling_params: Optional[SamplingParams] = None,
         pooling_params: Optional[PoolingParams] = None,
         num_speculative_tokens: int = 0,
-        block_id: int = 0,
     ) -> None:
         num_blocks = round_up(
             total_tokens,
             self.cache_config.block_size) // self.cache_config.block_size
         prompt_token_ids = list(range(total_tokens))
+        # the dummy block maintained by BlockPool (null_block)
+        null_block_id = 0
 
         req = NewRequestData(
             req_id=f"dummy_request_{len(requests)}",
@@ -1715,7 +1716,7 @@ class RBLNModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
             mm_features=[],
             sampling_params=sampling_params,
             pooling_params=pooling_params,
-            block_ids=([block_id] * num_blocks, ) * num_kv_cache_groups,
+            block_ids=([null_block_id] * num_blocks, ) * num_kv_cache_groups,
             num_computed_tokens=num_computed_tokens,
             lora_request=None,
         )
@@ -2086,7 +2087,6 @@ class RBLNModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
             pooling_params=PoolingParams(
                 task=self.get_supported_pooling_tasks()[0])
             if self.is_pooling_model else None,
-            block_id=self.cache_config.num_gpu_blocks - 1,
         )
         dummy_run_scheduler_output, _ = self._make_dummy_scheduler_outputs(
             dummy_run_requests, dummy_run_num_scheduled_tokens,
