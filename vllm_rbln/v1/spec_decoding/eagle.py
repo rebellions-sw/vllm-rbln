e0ca75601033ab6e9c6e95ea9c4a5327e6e9d0c7
changjun.lee <pord7457@gmail.com>
2026-02-10 09:15:13 +0000

    fix: pre-commit error

--- vllm_rbln/v1/spec_decoding/eagle.py
+++ vllm_rbln/v1/spec_decoding/eagle.py
@@ -197,9 +197,9 @@ def custom_propose(
     extra_attn_metadata_args = {}
     extra_attn_metadata_args["num_tokens"] = self.runner.input_batch.num_tokens_no_spec
     extra_attn_metadata_args["positions"] = target_positions.cpu()
-    extra_attn_metadata_args[
-        "batch_pad"] = self.runner.bucketing_manager.find_decode_batch_bucket(
-            batch_size)
+    extra_attn_metadata_args["batch_pad"] = (
+        self.runner.bucketing_manager.find_decode_batch_bucket(batch_size)
+    )
     attn_metadata = attn_metadata_builder.build(
         common_prefix_len=0,
         common_attn_metadata=common_attn_metadata,
@@ -386,8 +386,7 @@ def custom_propose(
         common_attn_metadata.seq_lens += 1
         # For the requests that exceed the max model length, we set the
         # sequence length to 1 to minimize their overheads in attention.
-        common_attn_metadata.seq_lens.masked_fill_(
-            exceeds_max_model_len.view(-1), 1)
+        common_attn_metadata.seq_lens.masked_fill_(exceeds_max_model_len.view(-1), 1)
 
         # Compute the slot mapping.
         if self.uses_mrope:
@@ -416,11 +415,13 @@ def custom_propose(
 
         # Rebuild attention metadata
         extra_attn_metadata_args = {}
-        extra_attn_metadata_args[
-            "num_tokens"] = common_attn_metadata.seq_lens.cpu().numpy()
+        extra_attn_metadata_args["num_tokens"] = (
+            common_attn_metadata.seq_lens.cpu().numpy()
+        )
         extra_attn_metadata_args["positions"] = positions.cpu()
-        extra_attn_metadata_args["batch_pad"] = \
+        extra_attn_metadata_args["batch_pad"] = (
             self.runner.bucketing_manager.find_decode_batch_bucket(batch_size)
+        )
         attn_metadata = attn_metadata_builder.build(
             common_prefix_len=0,
             common_attn_metadata=common_attn_metadata,
