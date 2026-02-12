7cedaebb53a14170dc4cac4b1a1329469ecda5f4
rebel-eunji <eunji.lee@rebellions.ai>
2026-02-12 13:21:52 +0900

    fix import

--- vllm_rbln/v1/sample/rbln_sampler.py
+++ vllm_rbln/v1/sample/rbln_sampler.py
@@ -19,10 +19,10 @@ from vllm.v1.sample.metadata import SamplingMetadata
 from vllm.v1.sample.sampler import Sampler as VLLMSampler
 import rebel
 from vllm.config.model import LogprobsMode
+from vllm.v1.outputs import LogprobsTensors, SamplerOutput
 from vllm_rbln.v1.sample.ops.penalties import (apply_all_penalties as
                                                rbln_apply_all_penalties)
 import vllm_rbln.rbln_envs as envs
-
 logger = init_logger(__name__)
 
 _SAMPLING_EPS = 1e-5
