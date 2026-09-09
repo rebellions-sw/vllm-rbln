b3bf2452118a537d11cdb7fa2ea7139ad3231bc0
rebel-eunji <eunji.lee@rebellions.ai>
2026-09-09 16:26:00 +0900

    pre-commit

--- tests/optimum/v1/sample/utils.py
+++ tests/optimum/v1/sample/utils.py
@@ -124,7 +124,12 @@ def make_request(
     )
 
 
-def get_vllm_config(async_scheduling=False, max_num_seqs=None, dtype=torch.float):
+def get_vllm_config(
+    async_scheduling=False,
+    max_num_seqs=None,
+    dtype=torch.float,
+    logprobs_mode="raw_logprobs",
+):
     max_model_len = MAX_MODEL_LEN
     max_num_batched_tokens = max(max_num_seqs, MAX_MODEL_LEN)
     scheduler_config = SchedulerConfig(
@@ -139,6 +144,7 @@ def get_vllm_config(async_scheduling=False, max_num_seqs=None, dtype=torch.float
         max_model_len=max_model_len,
         dtype=dtype,
         seed=42,
+        logprobs_mode=logprobs_mode,
     )
     cache_config = CacheConfig(
         block_size=OB_SIZE,
@@ -266,8 +272,11 @@ def create_model_runner(
     max_num_seqs: int = MAX_NUM_SEQ,
     dtype: torch.dtype = torch.float,
     decoder_batch_sizes: tuple[int, ...] | None = None,
+    logprobs_mode: str = "raw_logprobs",
 ):
-    vllm_config = get_vllm_config(max_num_seqs=max_num_seqs, dtype=dtype)
+    vllm_config = get_vllm_config(
+        max_num_seqs=max_num_seqs, dtype=dtype, logprobs_mode=logprobs_mode
+    )
     with set_current_vllm_config(vllm_config, check_compile=False):
         temp_file = tempfile.mkstemp()[1]
         init_distributed_environment(
